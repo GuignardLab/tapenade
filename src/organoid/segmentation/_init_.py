a200cb715924fa0df56cab174b9d23f524c3b6e7
aliceeeeeeeeee <alice.gros@univ-amu.fr>
2023-12-06 16:03:20 +0100

    first commit

---
+++ src/organoid/segmentation/_init_.py
@@ -0,0 +1,3 @@
+from ._segment import predict_stardist
+
+__all__ = ("predict_stardist",)
---
+++ src/organoid/segmentation/_segment.py
@@ -0,0 +1,37 @@
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+import numpy as np
+from csbdeep.utils import normalize
+from stardist import  random_label_cmap
+from stardist import random_label_cmap
+from stardist.models import StarDist3D
+np.random.seed(6)
+lbl_cmap = random_label_cmap
+
+from organoid.utils import change_voxelsize
+from pathlib import Path
+import os
+
+
+
+def predict_stardist(array, model_path:str,input_voxelsize:tuple=(1,1,1),normalize_input:bool=True) :
+    """
+    Predict the segmentation of an array using a StarDist model.
+    :param array: The array to segment.
+    :param model_path: The path to the StarDist model.
+    :param input_voxelsize: The voxel size of the input array.
+    :param normalize_input: Whether to normalize the input array.
+    :return: The predicted segmentation.
+    """
+
+    model_name = Path(model_path).stem
+    directory = str(os.path.split(model_path)[0])
+    model = StarDist3D(None,name=model_name,basedir=directory)
+
+    data = change_voxelsize(array,input_voxelsize,output_voxelsize=(0.7,0.7,0.7),order=1)
+    if normalize_input:
+        data = normalize(data,1,99)
+    labels,_ = model.predict_instances(data,axes='ZYX',n_tiles = model._guess_n_tiles(data))
+    aniso_labels = change_voxelsize(labels,input_vs=(0.7,0.7,0.7),output_vs=input_voxelsize, order=0)
+
+    return(np.asarray(aniso_labels).astype(np.int16))
