4253861d1a432ba3ef69dbac030a4c2614a9f2b2
aliceeeeeeeeee <alice.gros@univ-amu.fr>
2024-02-27 17:49:11 +0100

    removed reg test

--- src/organoid/reconstruction/_tests/test_reconstruction.py
+++ src/organoid/reconstruction/_tests/test_reconstruction.py
@@ -1,5 +1,5 @@
 from organoid.reconstruction import extract_positions,plot_positions,associate_top_bottom
 
-def test_position_array():
-    test_obj = reconstruct_foo()
-    assert test_obj == -1
+# def test_position_array():
+#     test_obj = reconstruct_foo()
+#     assert test_obj == -1
