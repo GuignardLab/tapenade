815216a0d7ddc88810608d0df7621031932f7334
aliceeeeeeeeee <alice.gros@univ-amu.fr>
2024-04-17 14:43:40 +0200

    no init

--- src/organoid/__init__.py
+++ src/organoid/__init__.py
@@ -1,8 +1,8 @@
 __version__ = "0.0.1"
 
-from ._organoid import Organoid, test_organoid
+# from .organoid import Organoid, test_organoid
 
-__all__ = [
-    "Organoid",
-    "test_organoid",
-]
+# __all__ = [
+#     "Organoid",
+#     "test_organoid",
+# ]
