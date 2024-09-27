5428883e77df87bb8d0120a536ac3506a886c019
aliceeeeeeeeee <alice.gros@univ-amu.fr>
2024-09-27 18:32:49 +0200

    with black

--- src/tapenade/reconstruction/__init__.py
+++ src/tapenade/reconstruction/__init__.py
@@ -13,15 +13,14 @@ from ._reconstruct import (
     remove_previous_files,
     sigmoid,
     write_hyperstacks,
-    compute_transformation_from_trsf_files
+    compute_transformation_from_trsf_files,
 )
 
 __all__ = (
     "extract_positions",
     "plot_positions",
     "associate_positions",
-    "transformation_from_plugin"
-    "manual_registration_fct",
+    "transformation_from_plugin" "manual_registration_fct",
     "register",
     "create_folders",
     "check_napari",
@@ -31,5 +30,5 @@ __all__ = (
     "write_hyperstacks",
     "add_centermass",
     "remove_previous_files",
-    "compute_transformation_from_trsf_files"
+    "compute_transformation_from_trsf_files",
 )
