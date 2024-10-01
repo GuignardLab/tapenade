6c7be1c091a6fcafd99b000fa3310f8be0f8a392
jules-vanaret <jules.vanaret@univ-amu.fr>
2024-10-01 19:16:52 +0200

    removed comments

--- src/tapenade/segmentation/_segment.py
+++ src/tapenade/segmentation/_segment.py
@@ -52,59 +52,6 @@ def predict_stardist(
     return aniso_labels.astype(np.int16)
 
 
-# def predict_stardist(
-#     array: np.ndarray,
-#     model_path: str,
-#     input_voxelsize: tuple,
-#     voxelsize_model: tuple = (0.7, 0.7, 0.7),
-#     normalize_input: bool = True,
-# ):
-#     """
-#     Predict the segmentation of an array using a StarDist model.
-
-#     Parameters:
-#     - array: a 3D numpy array, input image to segment
-#     - model_path: str, path to the StarDist model
-#     - input_voxelsize: tuple, input voxel size of the image
-#     - voxelsize_model: tuple, voxel size of the model
-#     - normalize_input: bool, whether to normalize the input image
-
-#     Returns:
-#     - aniso_labels: numpy array, predicted
-#     """
-#     assert len(np.shape(array)) <= 3
-#     model_name = Path(model_path).stem
-#     directory = str(os.path.split(model_path)[0])
-#     model = StarDist3D(None, name=model_name, basedir=directory)
-
-#     data = change_arrays_pixelsize(
-#         image=array,
-#         input_pixelsize=input_voxelsize,
-#         output_pixelsize=voxelsize_model,
-#         order=1,
-#     )
-#     if normalize_input:
-#         data = normalize(data, 1, 99)
-#     labels, _ = model.predict_instances(
-#         data, axes="ZYX", n_tiles=model._guess_n_tiles(data)
-#     )
-#     # aniso_labels = change_arrays_pixelsize(
-#     #     labels=labels,
-#     #     input_pixelsize=voxelsize_model,
-#     #     output_pixelsize=input_voxelsize,
-#     #     order=0,
-#     # )
-#     aniso_labels = resize(
-#         labels,
-#         array.shape,
-#         anti_aliasing=False,
-#         order=0,
-#         preserve_range=True,
-#     )
-
-#     return np.asarray(aniso_labels).astype(np.int16)
-
-
 def find_seg_errors(segmentation: np.ndarray, image: np.ndarray):
     """
     Compute statisitics of intensity of each label to try to find out exceptions/anomalies and detect segmentation errors.
